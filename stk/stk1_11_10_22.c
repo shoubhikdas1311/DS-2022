3b9726efe977fb2fe971f02f43d81e7d6a0e8224
Shoubhik Das <54733763+shoubhikdas1311@users.noreply.github.com>
2022-10-12 10:54:45 +0530

    Add files via upload

---
+++ stk/stk1_11_10_22.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define MAX_Count		100
+int isEmpty(char * stk, int * tos)
+{
+	return ((*tos) > 0);// Whether there's space for more
+}
+void push(char * stk, int * tos, char val)
+{
+	stk[(*tos)] = val;
+	/*
+			OR
+
+	scanf(" %c", (char *)(stk+(*tos)));
+	Can use another function that writes to stk
+	*/
+	(*tos)--;
+}
+char pop(char * stk, int * tos)
+{	
+	char ret;
+	ret = stk[(*tos)+1];// tos+1 to cancel out the old degradation 
+	stk[(*tos)+1] = 0;// freeing up the space	
+	(*tos)++;
+	return ret;
+}
+int main()
+{
+	int sz = 0, tos = 0, Max_sz = 0, i = 0;
+	int * tos_ptr = &tos;
+
+	char arr[MAX_Count];
+	char arr2[MAX_Count];
+	printf("Enter the size\n");
+	scanf("%d", &Max_sz);
+	/*
+	or hardcode
+	*/
+	tos = Max_sz;
+	push(arr, tos_ptr, '1');
+	push(arr, tos_ptr, '2');
+	push(arr, tos_ptr, '3');
+	printf("%c\n", pop(arr, tos_ptr));
+	printf("%d\n", tos);
+	printf("%d\n", isEmpty(arr, tos_ptr));
+	printf("The End");
+	return 0;
+}
